dbea81580ebceaeb05bc9d98e800695581498766
Layla Rodas <rodas.layla@gmail.com>
2024-03-27 09:49:24 +0100

    db and types folder created

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { MenuItem } from './components/MenuItem'
+import { menuItems } from './data/db'
 
 function App() {
 
@@ -6,6 +8,19 @@ function App() {
       <header className=" bg-teal-400 py-5">
         <h1 className=" text-center text-4xl font-black">Expense Calculator</h1>
       </header>
+
+      <main className=' max-w-7xl mx-auto py-20 grid md:grid-cols-2'>
+        <div>
+          <h2>Menu</h2>
+          {menuItems.map(item => (
+            <MenuItem key={item.id}  
+            />
+          ))}
+        </div>
+        <div>
+          <h2>Expenses</h2>
+        </div>
+      </main>
     </>
   )
 }
