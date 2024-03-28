89c3195ad72e4f40d8da73b4b29c1b0b6fe4751f
Layla Rodas <rodas.layla@gmail.com>
2024-03-28 10:50:00 +0100

    modif order state

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,12 +1,31 @@
 import { useState } from "react"
-import type { OrderItem } from "../types"
+import type { MenuItems, OrderItem } from "../types"
 
 export default function useOrder() {
 
     const [order, setOrder] = useState<OrderItem[]>([]);
-   
-    const addItem = () => {
-        console.log('add item')
+
+    const addItem = (item: MenuItems) => {
+
+
+        const itemExists = order.find(orderItem => orderItem.id === item.id)
+
+        if (itemExists) {
+            const updatedOrder = order.map(orderItem => orderItem.id === item.id ? { ...orderItem, quantity: orderItem.quantity + 1 } : orderItem)
+                
+                
+            /*{
+                if (orderItem.id === item.id) {
+                    return { ...orderItem, quantity: orderItem.quantity + 1 }
+                }
+                return orderItem
+            })*/
+            setOrder(updatedOrder)
+        } else {
+
+            const newItem = { ...item, quantity: 1 }
+            setOrder([...order, newItem])
+        }
     }
 
 
