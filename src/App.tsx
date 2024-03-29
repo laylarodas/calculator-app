8615b3ee6e302bda98fcd854aaa68c151a70ab9b
Layla Rodas <rodas.layla@gmail.com>
2024-03-29 19:44:13 +0100

    calculating order subtotal

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,9 @@ function App() {
             order={order}
             removeItem={removeItem}
           />
-          <OrderTotals />
+          <OrderTotals 
+            order={order}
+          />
         </div>
       </main>
     </>
--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,4 +1,16 @@
-export const OrderTotals = () => {
+import { useMemo } from "react";
+import { OrderItem } from "../types";
+import { formatCurrency } from "../helpers";
+
+type OrderTotalsProps = {
+  order: OrderItem[];
+};
+
+export const OrderTotals = ({order}: OrderTotalsProps) => {
+  
+  const SubtotalAmount = useMemo(() => order.reduce((total, item) => total + (item.quantity * item.price), 0), [order]);
+  
+  
   return (
     <>
       <div className="p-3 space-y-3">
@@ -7,7 +19,7 @@ export const OrderTotals = () => {
         </h2>
         <p>
           Subtotal: {''}
-          <span className="font-semibold"> $0.00</span>
+          <span className="font-semibold">{formatCurrency(SubtotalAmount)}</span>
         </p>
         <p>
           Tip: {''}
